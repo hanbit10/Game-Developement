d3f734988f8faf3bc6ffd4b5134c524b525355f9
hanbit10 <chanbit10@gmail.com>
2024-05-29 16:38:06 +0200

    Canvas: update values

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ playerImage.src = "./data/shadow_dog.png";
 let x = 0;
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 const spriteAnimations = [
@@ -29,17 +27,61 @@ const animationStates = [
     name: "jump",
     frames: 7,
   },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 4,
+  },
 ];
 
-animationStates.forEach(() => {});
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.fillRect(50, 50, 100, 100);
   // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-  let position = Math.floor(gameFrame / staggerFrames) % 6;
-  frameX = spriteWidth * position;
-  ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations["jump"].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations["jump"].loc[position].y;
+  ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
   if (gameFrame % staggerFrames == 0) {
     if (frameX < 6) frameX++;
