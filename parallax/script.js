f9a4d75633a749a8bc50a08957bcf18de4b54ed7
hanbit10 <chanbit10@gmail.com>
2024-06-01 14:57:24 +0200

    background update

--- parallax/script.js
+++ parallax/script.js
@@ -17,7 +17,7 @@ backgroundLayer5.src = "/data/background/layer-5.png";
 
 let gameSpeed = 5;
 
-class layer {
+class Layer {
   constructor(image, speedModifier) {
     this.x = 0;
     this.y = 0;
@@ -39,14 +39,35 @@ class layer {
     if(this.x2 <= -this.width ) {
       this.x2 = this.width + this.x2 - this.speed
     }
+
+    this.x = Math.floor(this.x - this.speed);
+    this.x2 = Math.floor(this.x2 - this.speed);
   }
 
   draw(){
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
   }
-
 }
+
+const layer1 = new Layer(backgroundLayer1, 0.5)
+const layer2 = new Layer(backgroundLayer2, 0.5)
+const layer3 = new Layer(backgroundLayer3, 0.5)
+const layer4 = new Layer(backgroundLayer4, 0.5)
+const layer5 = new Layer(backgroundLayer5, 0.5)
+
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  layer1.update()
+  layer1.draw()
+  layer2.update()
+  layer2.draw()
+  layer3.update()
+  layer3.draw()
+  layer4.update()
+  layer4.draw()
+  layer5.update()
+  layer5.draw()
   requestAnimationFrame(animate);
 }
 
